3fe70a8f537922f0139ecf8191191ac0a8963db4
vallabhi28-cpu <vallabhi28@gmail.com>
2026-05-26 00:02:36 +0530

    Update lab4(dining_threads).c

--- lab4(dining_threads).c
+++ lab4(dining_threads).c
@@ -1,70 +1,61 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <semaphore.h>
-
-#define BUFFER_SIZE 5
-
-int buffer[BUFFER_SIZE];
-int in = 0, out = 0;
-
-sem_t empty;           
-sem_t full;            
-pthread_mutex_t mutex; 
-
-void* producer(void* arg) {
-    int item, i = 0;
-    while (1) {
-        item = i++; 
-        
-        sem_wait(&empty);             
-        pthread_mutex_lock(&mutex);   
-        
-        buffer[in] = item;
-        printf("Produced: %d at buffer[%d]\n", item, in);
-        in = (in + 1) % BUFFER_SIZE;
-        
-        pthread_mutex_unlock(&mutex); 
-        sem_post(&full);             
-        
-        sleep(1); 
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#define N 5 
+pthread_mutex_t forks[N]; 
+pthread_t philosophers[N];
+void* philosopher(void* num)
+{
+int id = *(int*)num;
+int left = id; 
+int right = (id + 1) % N; 
+while (1)
+{
+    printf("Philosopher %d is thinking.\n", id);
+    sleep(1); 
+    if (id % 2 == 0)
+    {
+        pthread_mutex_lock(&forks[left]);
+        printf("Philosopher %d picked up left fork %d.\n", id, left);
+        pthread_mutex_lock(&forks[right]);
+        printf("Philosopher %d picked up right fork %d.\n", id, right);
     }
+    else
+    { 
+        pthread_mutex_lock(&forks[right]);
+        printf("Philosopher %d picked up right fork %d.\n", id, right); 
+        fork pthread_mutex_lock(&forks[left]);
+        printf("Philosopher %d picked up left fork %d.\n", id, left);
+    }
+    printf("Philosopher %d is eating.\n", id);
+    sleep(2); 
+    pthread_mutex_unlock(&forks[left]);
+    pthread_mutex_unlock(&forks[right]);
+    printf("Philosopher %d put down forks %d and %d.\n", id, left, right);
+}
+return NULL;
 }
 
-void* consumer(void* arg) {
-    int item;
-    while (1) {
-        sem_wait(&full);             
-        pthread_mutex_lock(&mutex);   
-        
-        item = buffer[out];
-        printf("Consumed: %d from buffer[%d]\n", item, out);
-        out = (out + 1) % BUFFER_SIZE;
-        
-        pthread_mutex_unlock(&mutex); 
-        sem_post(&empty);            
-        
-        sleep(2); 
+int main() 
+{
+    int i;
+    int ids[N]; 
+    for (i = 0; i < N; i++)
+    { 
+        pthread_mutex_init(&forks[i], NULL);
+        ids[i] = i;
     }
-}
-
-int main() {
-    printf("1BF24CS328 \n");
-    pthread_t prod, cons;
-   
-    sem_init(&empty, 0, BUFFER_SIZE);
-    sem_init(&full, 0, 0); 
-    pthread_mutex_init(&mutex, NULL);
-  
-    pthread_create(&prod, NULL, producer, NULL);
-    pthread_create(&cons, NULL, consumer, NULL);
-  
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
-    
-    sem_destroy(&empty);
-    sem_destroy(&full);
-    pthread_mutex_destroy(&mutex);
-    
+    for (i = 0; i < N; i++)
+    {
+        pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
+    } 
+    for (i = 0; i < N; i++)
+    {
+        pthread_join(philosophers[i], NULL); 
+    }
+    for (i = 0; i < N; i++)
+    {
+        pthread_mutex_destroy(&forks[i]);
+    }
     return 0;
 }
